e723bd24ec125f17f595fdd45fe832c642ddf9d7
Corentin BOLLAERT-LUCIANI <cor_bo@hotmail.fr>
2020-11-12 21:02:05 +0100

    Modif1

--- model/annuaire.py
+++ model/annuaire.py
@@ -1,41 +1,51 @@
 #imports
 from model.contact import *
+import json
 
 class Annuaire():
     def __init__(self):
         self.contacts=[]
-    def __SaveToJson__(self):
-        //TODO
-    def __LoadJSON__(self):
-        //TODO
-    def __addcontact__(self,contact):
+        
+    def saveToJson(self):
+        for contact in self.contacts:
+            contact.toJSON(self)
+
+    def loadJSON(self):
+        file = open("contacts.json", "r")
+        for contact in json.loads(file.read()):
+            __addcontact__(Contact(contact['firstname'],contact['lastname'],contact['number'],contact['departement'],contact['email']))
+        file.close()
+
+    def addContact(self,contact):
         self.contacts.append(contact)
-    def __showcontacts__(self):
+        
+    def showContacts(self):
         for contact in self.contacts:
             contact.__printInfos__(self)
-    def __removecontact__(self, index):
-        self.contacts.pop(index)
-    def __infocontact__(self, index):
+
+    # def removeContact(self, index):
+    #     self.contacts.pop(index)
+
+    def removeContact(self, contact):
+        self.contacts.remove(contact)
+
+    def infoContact(self, index):
+        
         self.contacts[index].__printInfos__()
-    def __getLength__(self):
+    def getLength(self):
         return len(self.contacts)
-    def __isNumAlreadyTaken__(self, num):
+
+    def isNumAlreadyTaken(self, num):
         for contact in self.contacts :
-            if num == contact.num:
+            if num.strip() == contact.num.strip():
                 return True
             return False
 
-        #Initialisation d'une liste de base
-        self.contacts = [Contact("Robard", "quentin.robard@sfr.fr", "78420", "06 15 15 15 15"), Contact("Bollaert", "cor_bo@hotmail.com", "16600", "06 12 12 12 12")]
+    def editContact(self, firstname, lastname, number, departement, email):
+        contactToEdit = self.contacts.__getitem__(contact)
+        contactToEdit.setFirstname(firstname)
+        contactToEdit.setLastname(lastname)
+        contactToEdit.setNumber(number)
+        contactToEdit.setDepartement(departement)
+        contactToEdit.setEmail(email)
 
-    def ajouterContact(self, contact):
-        self.contacts.append(contact)
-
-    def editerContact(self, contact, nom, email, dep, tel):
-        self.contacts.__getitem__(contact).setNom(nom)
-        self.contacts.__getitem__(contact).setEmail(email)
-        self.contacts.__getitem__(contact).setDep(dep)
-        self.contacts.__getitem__(contact).setTel(tel)
-
-    def supprimerContact(self, contact):
-        self.contacts.remove(contact)
--- model/contact.py
+++ model/contact.py
@@ -1,25 +1,26 @@
 class Contact():
     def __init__(self, firstname, lastname, number, departement, email):
-        self.number = number
-        self.lastname = lastname
         self.firstname = firstname
-        self.email = email
-        self.departement = departement
-
-    def setNumber(self, number):
+        self.lastname = lastname
         self.number = number
+        self.departement = departement
+        self.email = email
 
     def setFirstname(self, firstname):
         self.firstname = firstname
-
+        
     def setLastname(self, lastname):
         self.lastname = lastname
+
+    def setNumber(self, number):
+        self.number = number
 
     def setDepartement(self, departement):
         self.departement = departement
 
     def setEmail(self, email):
         self.email = email
+        
 
     def toJSON(self):
         return "{"+\
