41b8aae214a8052b5a3f3b2c3f71e2fddbae9645
Quentin Robard <quentin.robard@etu.univ-lehavre.fr>
2020-11-12 22:31:07 +0100

    joli dialog

--- view/contactForm.py
+++ view/contactForm.py
@@ -1,4 +1,4 @@
-from PyQt5.QtWidgets import QMainWindow
+from PyQt5.QtWidgets import *
 
 
 class ContactForm(QMainWindow):
@@ -7,14 +7,39 @@ class ContactForm(QMainWindow):
 
         # Init de la fenetre
         self.setWindowTitle(title)
-        self.setGeometry(1000, 300, 300, 500)
+        self.setGeometry(1000, 300, 300, 200)
         # empecher le resize
         self.setFixedSize(self.size())
         self.statusBar().setSizeGripEnabled(False)
 
         self.initUi()
 
-
-
     def initUi(self):
-        print('yo')
+        mainLayout = QVBoxLayout()
+
+        #Layout du formulaire
+        formLayout = QFormLayout()
+
+        #Ajout des lignes
+        formLayout.addRow(QLabel("firstname"), QLineEdit())
+        formLayout.addRow(QLabel("lastname"), QLineEdit())
+        formLayout.addRow(QLabel("number"), QLineEdit())
+        formLayout.addRow(QLabel("departement"), QLineEdit())
+        formLayout.addRow(QLabel("email"), QLineEdit())
+
+        mainLayout.addLayout(formLayout)
+
+        #Layout des boutons
+        btnLayout = QHBoxLayout()
+
+        #Ajout des boutons
+        self.validate = QPushButton("Valider")
+        self.cancel = QPushButton("Annuler")
+        btnLayout.addWidget(self.validate)
+        btnLayout.addWidget(self.cancel)
+
+        mainLayout.addLayout(btnLayout)
+
+        window = QWidget()
+        window.setLayout(mainLayout)
+        self.setCentralWidget(window)
